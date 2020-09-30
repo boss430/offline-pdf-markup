d6b984cf7f715fcee5096e14acd306ad445bbb96
boss430 <31330068+boss430@users.noreply.github.com>
2020-09-30 11:32:00 +0700

    Use upsert insteadof save in post svg

--- docs/mongo.js
+++ docs/mongo.js
@@ -3,25 +3,26 @@ var router = express.Router();
 
 const Model = require('./mongoSchema.js');
 /* GET users listing. */
-router.post('/', function(req, res, _next) {
+router.post('/', function (req, res, _next) {
     const payload = {
         name: req.body.name,
         svg: req.body.svg
     }
-    new Model(payload).save((err, data)=>{
-        if(err){
+
+    Model.updateOne({ name: payload.name }, { svg: payload.svg }, { upsert: true }, (err, data) => {
+        if (err) {
             res.sendStatus(500);
-        }else{
-            res.status(200).send(data);
+        } else {
+            res.sendStatus(200);
         }
-    });
+    })
 });
 
-router.get('/', (req, res, _next)=>{
-    Model.find({name: req.query.name}, { __v: 0, _id: 0 }).exec((err, data)=>{
-        if(err){
+router.get('/', (req, res, _next) => {
+    Model.find({ name: req.query.name }, { __v: 0, _id: 0 }).exec((err, data) => {
+        if (err) {
             res.sendStatus(500)
-        }else{
+        } else {
             res.status(200).send(data[0])
         }
     })
